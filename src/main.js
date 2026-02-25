f46b6c214f9cdde79a27c1d755460a92d8c65cac
PedroPS <pedroperezsaez.1992@gmail.com>
2026-02-25 18:29:04 +0100

    algunos planetas hechos

--- src/main.js
+++ src/main.js
@@ -10,27 +10,70 @@ canvas.height = innerHeight;
 
 
 let angulo = 0; 
-
+let anguloMercurio= 0;
+let anguloTierra = 10;
+let anguloVenus = 0;
 function animar(){
 ctx.fillStyle ='#020b1a';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 draw()           
-
-  angulo = angulo + 0.01;
+anguloMercurio= anguloMercurio + 0.04
+ anguloTierra = anguloTierra + 0.04
+ anguloVenus = anguloVenus + 0.01
   requestAnimationFrame(animar);
+  
+}
+function dibujarVenus(){
+  ctx.save()
+  ctx.translate(centrox,centroy)
+  ctx.rotate(anguloVenus)
+  ctx.beginPath()
+  ctx.arc(250,0,20,0,10)
+  ctx.strokeStyle = "white";
+  ctx.fillStyle='#FFC375';
+  ctx.fill()
+  ctx.restore()
+}
+function dibujarTierra(){
+ctx.save()
+ctx.translate(centrox,centroy);
+  ctx.rotate(anguloTierra)
+  ctx.beginPath();
+  ctx.arc(350,0,20,0,10)
+  ctx.fillStyle='#2761F5'
+  ctx.fill()
+  ctx.restore()
 }
 
-
+function dibujarMercurio(){
+ ctx.save
+  ctx.translate(centrox,centroy);
+  ctx.rotate(anguloMercurio)
+  ctx.beginPath();
+  ctx.arc(150,0,8,0,10)
+  ctx.fillStyle = '#df2525';  
+  ctx.fill()
+  ctx.restore();
+ 
+}
 function dibujarSol(){
+  ctx.save()
     ctx.beginPath();
-  ctx.arc(centrox, centroy, 40, 0, 10);
+  ctx.arc(centrox, centroy, 80, 0, 10);
   ctx.fillStyle = 'yellow'; 
   ctx.fill();  
+  ctx.restore()
 }
 function draw(){
-  
+   ctx.save()
 dibujarSol();
+dibujarMercurio();
+
+dibujarTierra();
+dibujarVenus();
+
+ ctx.restore()
 
 }
 
